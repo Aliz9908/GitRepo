59245d2a2e2a607f44f505ee5b8845524143a18e
Aliz9908 <alizdiscord9908@gmail.com>
2022-09-23 06:09:51 -0700

    test commit on github

--- app/src/main/java/com/example/prijectgit/MainActivity.kt
+++ app/src/main/java/com/example/prijectgit/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.prijectgit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.prijectgit.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.tv1.text = "Hello github"
+        binding.tv1.setOnClickListener {
+            binding.tv1.visibility = View.INVISIBLE
+        }
     }
 }
